7ffa709c8073b7b4f9889afb372525a07ebda346
Brian Bonnlander <bonnland@ucar.edu>
2022-06-30 22:06:45 +0000

    DSET-343 Update datepicker and change default dates

--- ckanext/datesearch/fanstatic/daterangepicker-module.js
+++ ckanext/datesearch/fanstatic/daterangepicker-module.js
@@ -43,22 +43,24 @@ this.ckan.module('daterangepicker-module', function ($) {
             $('#start').datepicker({
 		        format: "yyyy-mm-dd",
 		        startDate: earliest_date_str,
+                        clearBtn: true,
+                        defaultViewDate: { year: 1850, month: 01, day: 25 },
                         //forceParse: false,
                         autoclose: true 
 	        }).on('changeDate', function (ev) {
 	            var fs = 'YYYY-MM-DDTHH:mm:ss';
 	            //var fs = 'YYYY-MM-DD';
+                var default_date_str = "1850-01-25";
                 var start_date = moment(ev.date);
                 var start_date_str = start_date.format('YYYY-MM-DD');
                 var end_date_str = $('#end').val();
                 var end_date = moment(end_date_str, "YYYY-MM-DD");
 
-                if (start_date.isValid() && start_date > earliest_date) {
+                if (start_date.isValid() && start_date_str != default_date_str) {
 
                    //Flip the dates if end date is before start date, this ensures correct order in search view for results.
                    if(end_date.isValid() && (start_date > end_date)) {
                        $('#ext_startdate').val(end_date.format(fs) + 'Z');
-//                       start_date.add('y', 1).subtract('s', 1);
                        $('#ext_enddate').val(start_date.format(fs) + 'Z');
                    }
                    else {
@@ -72,24 +74,26 @@ this.ckan.module('daterangepicker-module', function ($) {
             $('#end').datepicker({
 		        format: "yyyy-mm-dd",
 		        startDate: earliest_date_str,
+                        clearBtn: true,
+                        defaultViewDate: { year: 2100, month: 01, day: 25 },
                         //forceParse: false,
                         autoclose: true
 	        }).on('changeDate', function (ev) {
 	            var fs = 'YYYY-MM-DDTHH:mm:ss';
 	            //var fs = 'YYYY-MM-DD';
+                var default_date_str = "2100-01-25";
                 var end_date = moment(ev.date);
+                var end_date_str = end_date.format('YYYY-MM-DD');
                 var start_date_str = $('#start').val();
                 var start_date = moment(start_date_str, "YYYY-MM-DD");
-                if (end_date.isValid() && end_date > earliest_date) {
+                if (end_date.isValid() && end_date_str != default_date_str) {
 
                    //Flip the dates if end date is before start date, this ensures correct order in search view for results.
                    if(start_date.isValid() && (end_date < start_date)) {
                        $('#ext_startdate').val(end_date.format(fs) + 'Z');
-//                       start_date.add('y', 1).subtract('s', 1);
                        $('#ext_enddate').val(start_date.format(fs) + 'Z');
                    }
                    else {
-//                       end_date.add('y', 1).subtract('s', 1);
                        $('#ext_enddate').val(end_date.format(fs) + 'Z');
                    }
                    form.submit();
